18e91305243566a33ff29b19302b59fa6592f6c8
Sajalmahapatra <sajalmahapatra94@gmail.com>
2023-06-25 22:07:11 +0530

    added first home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
 import './App.css';
+import Home from './pages/Home';
+import { darkTheme, lightTheme } from './theme';
+import { ThemeProvider } from 'styled-components';
+
 
 function App() {
   console.log(process.env.REACT_APP_WEATHER_API_KEY);
+  const darkMode = useSelector((state) => state.app.darkMode);
   return (
-    <div className="">
-
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Home/>
+    </ThemeProvider>
   );
 }
 
