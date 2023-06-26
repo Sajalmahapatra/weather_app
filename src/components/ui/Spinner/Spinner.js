6e49ddd0c8b1b4650f7b877f2f8ee409aeb48b76
Sajalmahapatra <sajalmahapatra94@gmail.com>
2023-06-26 10:25:29 +0530

    formated spinner loader

--- src/components/ui/Spinner/Spinner.js
+++ src/components/ui/Spinner/Spinner.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Spinner.styles.css';
+import React from "react";
+import "./Spinner.styles.css";
 
 const Spinner = () => {
   return (
     <div className="loader">
       <div className="sk-chase">
-        {
-            Array(6).fill(1).map((item, i) => <div className="sk-chase-dot" key={i}/>)
-        }
+        {Array(6)
+          .fill(1)
+          .map((item, i) => (
+            <div className="sk-chase-dot" key={i} />
+          ))}
       </div>
     </div>
   );
