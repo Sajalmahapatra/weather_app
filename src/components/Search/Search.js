ee643e4edb81ef5f82ed91d4c275d117fe678046
Sajalmahapatra <sajalmahapatra94@gmail.com>
2023-06-29 00:54:13 +0530

    add debouncing and geo location

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { SearchElement, SearchIcon, SearchInput } from './styled'
+import React, { useState } from "react";
+import {
+  LocationButton,
+  LocationIcon,
+  SearchElement,
+  SearchIcon,
+  SearchInput,
+} from "./styled";
+import { useDispatch } from "react-redux";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  let timer;
+  const [search, setSearch] = useState("");
+
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      setSearch(value);
+    }, 300);
+  };
+
+  const showPosition = (position) => {
+    const { latitude, longitude } = position.coords;
+  };
   return (
     <SearchElement>
-        <SearchIcon/>
-        <SearchInput/>
+      <SearchIcon />
+      <SearchInput onChange={handleChange} />
+      <LocationButton
+        onClick={() => {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+            alert("Geolocation is not supported by this browser.");
+          }
+        }}
+      >
+        <LocationIcon />
+      </LocationButton>
     </SearchElement>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,22 +1,23 @@
 import styled from "styled-components";
 import { ReactComponent as SearchIconSvg } from "../../assets/searchIcon.svg";
+import { ReactComponent as LocationIconSvg } from '../../assets/locationIcon.svg';
 
 export const SearchElement = styled.div`
   position: relative;
   height: 3.25rem;
   border-radius: 26px;
-  background: ${({ theme }) => theme.panelBgColor};
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
   margin-bottom: 1.4rem;
   display: flex;
   align-items: center;
   z-index: 1;
+  background-color: ${({ theme }) => theme.panelBgColor};
 `;
 export const SearchInput = styled.input`
   flex: 1;
   border-radius: 9999px;
-  margin-left: 1rem;
-  height: 3.25rem;
+  padding-left:10px;
+  height: 100%;
   border: none;
   background-color: ${({ theme }) => theme.panelBgColor};
   font-size: 1.125rem;
@@ -31,5 +32,18 @@ export const SearchInput = styled.input`
 `;
 export const SearchIcon = styled(SearchIconSvg)`
   margin-left: 1.2rem;
+  fill: #4a6fa1;
+`;
+export const LocationButton = styled.button`
+ border: none;
+ cursor: pointer;
+ background-color: transparent;
+ height: 100%;
+ display: contents;
+ &:hover svg{
+ }
+`;
+export const LocationIcon = styled(LocationIconSvg)`
+  margin-right: 1.2rem;
   fill: #4a6fa1;
 `;
