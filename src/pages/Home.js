8c395b8fca5a53b6419ab605e0328c9c9e48d18d
Sajalmahapatra <sajalmahapatra94@gmail.com>
2023-06-30 00:41:45 +0530

    create a currnt weather component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Spinner from '../components/ui/Spinner/Spinner'
 import Header from '../components/Header'
 import Search from '../components/Search/Search'
 import Footer from '../components/Footer/Footer'
+import CurrentWeather from '../components/CurrentWeather/CurrentWeather'
 
 const Home = () => {
     
@@ -11,6 +12,7 @@ const Home = () => {
     {/* <Spinner/> */}
     <Header/>
     <Search/>
+    <CurrentWeather/>
     <Footer />
     </>
   )
