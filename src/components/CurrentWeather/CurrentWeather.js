f826bef8e3d696b6b69488d0911eb591223dc288
Sajalmahapatra <sajalmahapatra94@gmail.com>
2023-07-02 17:22:17 +0530

    render weather data in the card with icons

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,16 +1,26 @@
 import React, { memo, useEffect } from "react";
 import {
   CurrentWeatherContainer,
+  CurrentWeatherInfo,
   CurrentWeatherStatus,
+  FeelsLike,
+  HighLowContainer,
+  InfoRow,
   SectionTitle,
   WeatherContainer,
+  WeatherDegree,
 } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTempUnit } from "../../store/reducers/appReducer";
 import ToggleSwitch from "../ui/ToggleSwitch/ToggleSwitch";
 import WeatherIcon from "./WeatherIcon";
 import Temperature from "./Temperature";
-// import { changeTempUnit } from '../../store/reducers/appReducer';
+import { ReactComponent as HighIcon } from "../../assets/highIcon.svg";
+import { ReactComponent as LowIcon } from "../../assets/lowIcon.svg";
+import { ReactComponent as HumidityIcon } from "../../assets/humidityIcon.svg";
+import { ReactComponent as PressureIcon } from "../../assets/pressureIcon.svg";
+import { ReactComponent as WindIcon } from "../../assets/windIcon.svg";
+import { TempUnit, kmToMile } from "../../utils/unitConversion";
 
 const CurrentWeather = () => {
   const { weather, degreeType, isInitial, isError } = useSelector((store) => {
@@ -47,6 +57,47 @@ const CurrentWeather = () => {
           </div>
           <h6>{weather.weather.description}</h6>
         </CurrentWeatherStatus>
+        <CurrentWeatherInfo>
+          <FeelsLike>
+            Feels like <Temperature value={weather.main.feels_like} />
+            <sup>&deg;</sup>
+          </FeelsLike>
+          <HighLowContainer>
+            <WeatherDegree>
+              <HighIcon />
+              <Temperature value={weather.main.temp_max} />
+              <sup>&deg;</sup>
+            </WeatherDegree>
+            <WeatherDegree>
+              <LowIcon />
+              <Temperature value={weather.main.temp_min} />
+              <sup>&deg;</sup>
+            </WeatherDegree>
+          </HighLowContainer>
+          <InfoRow>
+            <div>
+              <HumidityIcon /> Humidity
+            </div>
+            <span>{weather.main.humidity}%</span>
+          </InfoRow>
+          <InfoRow>
+            <div>
+              <WindIcon /> Wind
+            </div>
+            <span>
+              {degreeType === TempUnit.CELCIUS
+                ? weather.wind.speed
+                : kmToMile(weather.wind.speed)}
+              {degreeType === TempUnit.CELCIUS ? "kph" : "mph"}
+            </span>
+          </InfoRow>
+          <InfoRow>
+            <div>
+              <PressureIcon /> Pressure
+            </div>
+            <span>{weather.main.pressure}hPa</span>
+          </InfoRow>
+        </CurrentWeatherInfo>
       </CurrentWeatherContainer>
     </WeatherContainer>
   );
